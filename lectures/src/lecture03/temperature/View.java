79608116d2fa52e10b273f93e3cb873b71cadad1
PARKHAMIN\82102 <jqkrgkals123@gmail.com>
2024-10-03 21:43:05 +0900

    Model.ctof 메소드 완성

--- lectures/src/lecture03/temperature/View.java
+++ lectures/src/lecture03/temperature/View.java
@@ -1,9 +1,7 @@
 package lecture03.temperature;
 
 public class View {
-    double getTemperature() {
-        return 0.0;
-    }
+    double getTemperature() { return 0.0; }
 
     void showTemperature(double c, double f) {
         System.out.println("섭씨 " + c + "도를 화씨로 바꾸면 " + f + "도입니다.");
